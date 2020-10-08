6799cec36601df49fcaa45161773fe94bc3e87e6
kalinkinajane <62453022+kalinkinajane@users.noreply.github.com>
2020-10-08 21:03:17 +1100

    project 10: исправлены ошабки обьед метод Likes

---
+++ src/utils/api.js
@@ -0,0 +1,111 @@
+import { Promise } from "core-js";
+
+export class Api {
+    constructor({ url, headers }) {
+        this._url = url;
+        this._headers = headers;
+    }
+    getItems() {
+        return fetch(this._url, {
+            method: "GET",
+            headers: this._headers
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        });
+    }
+
+    getUserInfo() {
+        return fetch('https://mesto.nomoreparties.co/v1/cohort-15/users/me', {
+            method: "GET",
+            headers: this._headers
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        })
+    }
+    getAllNeedData() {
+        return Promise.all([this.getItems(), this.getUserInfo()])
+    }
+    patchUserInfo(data) {
+        return fetch('https://mesto.nomoreparties.co/v1/cohort-15/users/me', {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                name: data.name,
+                about: data.about
+            })
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        })
+
+    }
+    editAvatar(link) {
+        return fetch('https://mesto.nomoreparties.co/v1/cohort-15/users/me/avatar', {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                avatar: link.avatar
+            })
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        })
+    }
+    addnewCard(data) {
+        return fetch(this._url, {
+            method: "POST",
+            headers: this._headers,
+            body: JSON.stringify({
+                name: data.name,
+                link: data.link
+            })
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        })
+    }
+    removeCard(id) {
+        return fetch(`${this._url}/${id}`, {
+            method: "DELETE",
+            headers: this._headers,
+        }).then((res) => {
+
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        });
+    }
+    toggleLikes(id, like){
+        return fetch(`https://mesto.nomoreparties.co/v1/cohort-15/cards/likes/${id}`, {
+            method: like ? 'PUT' : 'DELETE',
+            headers: this._headers,
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`)
+        });
+    }
+}
+
+const api = new Api({
+    url: 'https://mesto.nomoreparties.co/v1/cohort-15/cards',
+    headers: {
+        authorization: '26d86045-320c-4f7c-aeb9-3bada9d26c3e',
+        'Content-Type': 'application/json',
+    }
+})
+export default api;
